1e1d0749cb9ff9fa9ee7457ff9cb5ef7f9c59ef6
Felix Lange <fjl@twurst.com>
2018-06-07 10:04:26 +0200

    p2p/enode: fix UDP/TCP port mixup

--- p2p/enode/urlv4.go
+++ p2p/enode/urlv4.go
@@ -1,3 +1,19 @@
+// Copyright 2018 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package enode
 
 import (
@@ -124,7 +140,7 @@ func parseComplete(rawurl string) (*Node, error) {
 			return nil, errors.New("invalid discport in query")
 		}
 	}
-	return newNodeV4(id, ip, int(udpPort), int(tcpPort)), nil
+	return newNodeV4(id, ip, int(tcpPort), int(udpPort)), nil
 }
 
 // parsePubkey parses a hex-encoded secp256k1 public key.
